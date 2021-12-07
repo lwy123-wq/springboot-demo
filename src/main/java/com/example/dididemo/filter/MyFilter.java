6603dcef84b77c6a132cfefa3db9dd1d123d7b53
lwy123-wq <l2158223895@qq.com>
2021-12-07 21:29:48 +0800

    demo

--- src/main/java/com/example/dididemo/filter/MyFilter.java
+++ src/main/java/com/example/dididemo/filter/MyFilter.java
@@ -3,11 +3,13 @@ package com.example.dididemo.filter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.ComponentScan;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
 
 public class MyFilter implements Filter {
     private Logger logger =LoggerFactory.getLogger(MyFilter.class);
