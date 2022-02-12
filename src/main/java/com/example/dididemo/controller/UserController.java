efba57470244a1687fbd22ea03c79fc7d5d83389
lwy123-wq <l2158223895@qq.com>
2022-02-12 19:56:53 +0800

    demo

--- src/main/java/com/example/dididemo/controller/UserController.java
+++ src/main/java/com/example/dididemo/controller/UserController.java
@@ -79,14 +79,13 @@ public class UserController {
     }
 
 
-    @ResponseBody
+    /*@ResponseBody
     @PostMapping("/uploadESignatureImg")
     public void uploadESignatureImg(MultipartFile file, HttpServletRequest request, HttpServletResponse response) {
-        /*String filePath = uploadESignature(file);
+        *//*String filePath = uploadESignature(file);
 
-        return renderSuccess(filePath);*/
-        System.out.println(file+"aaaaaaaaaaa");
-    }
+        return renderSuccess(filePath);*//*
+    }*/
     @RequestMapping(value = "/picture", method = RequestMethod.GET)
     public String picture() {
         return "picture";
