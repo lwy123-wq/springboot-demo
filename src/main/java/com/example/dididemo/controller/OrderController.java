25bd6c92d6429d5aeb295570392e5152c6a86885
lwy123-wq <l2158223895@qq.com>
2022-02-28 23:43:30 +0800

    demo

--- src/main/java/com/example/dididemo/controller/OrderController.java
+++ src/main/java/com/example/dididemo/controller/OrderController.java
@@ -16,9 +16,7 @@ public class OrderController {
     //调用支付服务
     @Resource
     private PayService payService;
-
     /*阿里支付*/
-
     /*@PostMapping(value = "alipay")
     public String alipay(String out_trade_no,String subject,String total_amount,String body) throws AlipayApiException {
         return  payService.aliPay(new AlipayBean()
